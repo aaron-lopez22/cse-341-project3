7b56221283a350ecfeaec5dec2eeff2216110868
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2025-07-27 14:49:41 -0600

    mongodbsetup

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
+const mongodb = require('./data/database');
+const e = require('express');
+
+
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/', require('./routes'));
 
+mongodb.initDB((err) => {
+  if (err) {
+    console.log('Failed to connect to the database');
+    console.error(err);
+  } else {
+    console.log('Connected to the database and listening on port', port);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
