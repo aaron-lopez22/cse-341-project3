907fc8f303f25b266bb78d2e5876aedc428269bd
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2025-07-29 21:55:14 -0600

    renderprep

--- routes/games.js
+++ routes/games.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/games');
-const { use } = require('react');
+
 
 router.get('/', usersController.getAll);
 
